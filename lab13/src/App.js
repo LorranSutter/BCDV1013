b8fc469f6b49866a26f04f9aaacaf2f7f869252b
Lorran Sutter <sutterlorran@gmail.com>
2020-07-28 11:57:32 -0400

    Lab 13 BONUS styling

--- lab13/src/App.js
+++ lab13/src/App.js
@@ -24,6 +24,68 @@ class TodoApp extends React.Component {
 			items: [],
 		};
 
+		this.wrapper = {
+			margin: '20px',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center'
+		};
+
+		this.content = {
+			width: '45%',
+			padding: '10px',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			height: '75%',
+			borderRadius: '10px',
+			boxShadow: '2px 2px 10px #AAAAAA'
+		};
+
+		this.inputContent = {
+			margin: '1em',
+		};
+
+		this.inputStyle = {
+			boxSizing: 'border-box',
+			padding: '0 1em',
+			height: '40px',
+			width: '400px',
+			borderRadius: '5px',
+			border: '1px solid #AAAAAA',
+			outline: 'none'
+		};
+
+		this.selectStyle = {
+			margin: '0 1em',
+			padding: '0 1em',
+			boxSizing: 'border-box',
+			height: '40px',
+			width: '100px',
+			borderRadius: '5px',
+			border: '1px solid #AAAAAA',
+			outline: 'none'
+		};
+
+		this.buttonStyle = {
+			boxSizing: 'border-box',
+			height: '40px',
+			width: '100px',
+			borderRadius: '5px',
+			border: 'none',
+			outline: 'none'
+		};
+
+		this.tableStyle = {
+			width: '100%',
+		};
+
+		this.tableHeaderStyle = {
+			padding: '10px',
+			color: '#F9F9F9',
+			background: '#AAAAAA'
+		};
+
 		this.refreshItems();
 	}
 
@@ -72,35 +134,77 @@ class TodoApp extends React.Component {
 	}
 
 	render() {
-		return <div>
-			<fieldset>
-				<legend>Add TODO Item</legend>
-				<input ref="description" placeholder="description" />
-				<select ref="priority" defaultValue="medium">
-					<option value="low">low</option>
-					<option value="medium">medium</option>
-					<option value="high">high</option>
-				</select>
-				<button onClick={() => this.addItem()}>Add Item</button>
+		return (
+			<div style={this.wrapper}>
+				<div style={this.content}>
 
-				<div style={{ color: 'red' }}>{this.state.addError}</div>
-			</fieldset>
+					<h1>Add TODO Item</h1>
+					<div style={this.inputContent}>
+						<input ref="description" placeholder="description" style={this.inputStyle} />
+						<select ref="priority" defaultValue="medium" style={this.selectStyle}>
+							<option value="low">low</option>
+							<option value="medium">medium</option>
+							<option value="high">high</option>
+						</select>
+						<button onClick={() => this.addItem()} style={this.buttonStyle}>Add Item</button>
+					</div>
 
-			<div>
-				{this.state.items.map((item) => <TodoItem item={item} key={item.id} onCompleted={() => this.completeItem(item.id)} />)}
+					<div style={{ color: 'red' }}>{this.state.addError}</div>
+
+					<table style={this.tableStyle}>
+						<th style={this.tableHeaderStyle}>Description</th>
+						<th style={this.tableHeaderStyle}>Priority</th>
+						<th style={this.tableHeaderStyle}>Status</th>
+						{this.state.items.map((item) => (
+							<TodoItem
+								item={item}
+								key={item.id}
+								onCompleted={() => this.completeItem(item.id)}
+							/>)
+						)}
+					</table>
+				</div>
 			</div>
-		</div>;
+		);
 	}
 }
 
 
 function TodoItem(props) {
-	let status = props.item.status;
-	if (status === 'open') status = <button onClick={props.onCompleted}>Completed</button>;
+	const buttonStyle = {
+		boxSizing: 'border-box',
+		border: 'none',
+		borderRadius: '5px',
+		height: '30px',
+		outline: 'none',
+		background: '#AAAAAA'
+	};
 
-	return <div>
-		{props.item.description} / {props.item.priority} / {status}
-	</div>;
+	const todoRowStyle = {
+		textAlign: 'center',
+		background: '#F2F2F2'
+	};
+
+	const todoColumnStyle = {
+		padding: '10px 0'
+	};
+
+	let status = props.item.status;
+	if (status === 'open') status = <button onClick={props.onCompleted} style={buttonStyle}>Completed</button>;
+
+	return (
+		<tr style={todoRowStyle}>
+			<td style={todoColumnStyle}>
+				{props.item.description}
+			</td>
+			<td style={todoColumnStyle}>
+				{props.item.priority}
+			</td>
+			<td style={todoColumnStyle}>
+				{status}
+			</td>
+		</tr>
+	);
 }
 
 
